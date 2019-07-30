f147943f0bf2e365d8de99b6af454efe99159738
nenad77 <josic.nenad@gmail.com>
2019-07-30 18:26:18 +0200

    Create React App

---
+++ my-app/src/App.js
@@ -0,0 +1,28 @@
+import React, {Component, Fragment} from 'react';
+import './App.css';
+import data from "./data/data.json";
+
+import {Header} from "./components/Header/Header";
+import {Cards} from "./components/Cards/Cards";
+
+
+class App extends Component {
+
+  onCardRemove = (e) => {
+    console.log(e.target)
+  }
+
+  render() {
+      return (
+        <Fragment>
+        <Header pageName="About" widthClass="full-width"/>
+        <div className="main-content">
+          <Cards data={data} removeCard={this.onCardRemove}/>
+        </div>
+        </Fragment>
+      );
+    }
+}
+
+
+export default App;
